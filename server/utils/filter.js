e67c7e216912c876040e4dc1f4aacd45121072c5
Jose Starsky Naya Lara <starsky@Joses-MBP.home>
2018-02-24 15:41:24 +0100

    hong kong bounding box

--- server/utils/filter.js
+++ server/utils/filter.js
@@ -3,6 +3,6 @@
  */
 
 // http://boundingbox.klokantech.com/
-exports.locations = '0.0549,40.4156,3.642,42.9846';
+exports.locations = '113.5812810646126536,22.1968401402418110,114.2599835932713290,22.6545823666874746';
 
 exports.track = ['Mobilització', 'ciutadana','Mobilitzacions','MobilitzacióCiutadana','Espais', 'oberts','EspaisOberts','Planejament municipal','PlanejamentMunicipal','Estudis','CreixementUrbà','TransformacióUrbana','Equipaments','PlaSectorial','PlaEspecial','ENP','EspaisNaturals','Carreteres','Legislació','Tren','PlanejamentTerritorial','Directrius','Habitatge','urbanisme','ecologiaurbana','urban','urbanplanning','cities','mediambient','ecologia','buitsurbans','ActivitatEconòmica','Energia','Residus','Aigua','Paisatge','Contaminació','RiscosNaturals','Ports','Aeroports','OrganitzacióTerritorial','RecursosNaturals','Participació','Mobilitat','Ciutat','Micropobles','Camins','Local','AgendaUrbana','Territori','MónRural','Barris','Turisme'];
